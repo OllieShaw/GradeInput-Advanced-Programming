4cb71ea037eda3220d9eb80b594a59102068206d
Oliver Shaw <OllieS@portland-fuel.co.uk>
2026-05-06 11:57:29 +0100

    Add test project with unit tests for UAppGradeController
    Added GradeInput-Advanced-Programming.Tests project with xUnit, Moq, and EF Core InMemory for unit testing. Updated main project and solution files to include test dependencies. Implemented UAppGradeControllerTests to verify AddSplitRow, Create, and Edit actions using an in-memory database context.

---
+++ GradeInput-Advanced-Programming.Tests/UAppGradeControllerTests.cs
@@ -0,0 +1,90 @@
+﻿using Xunit;
+using Microsoft.EntityFrameworkCore;
+using GradeInput_Advanced_Programming.Controllers;
+using GradeInput_Advanced_Programming.Trading;
+using Models.UappGrade;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+public class UAppGradeControllerTests
+{
+    private TradingContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<TradingContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb")
+            .Options;
+
+        return new TradingContext(options);
+    }
+
+    [Fact]
+    public void AddSplitRow_FirstRow_ShouldBe100Percent()
+    {
+        var context = GetDbContext();
+        var controller = new UAppGradeController(context);
+
+        var vm = new UappGradeVm
+        {
+            SplitData = new List<SplitData>()
+        };
+
+        var result = controller.AddSplitRow(vm) as ViewResult;
+        var model = result.Model as UappGradeVm;
+
+        Assert.Single(model.SplitData);
+        Assert.Equal(100m, model.SplitData.First().Ratio);
+    }
+
+    [Fact]
+    public void AddSplitRow_SecondRow_ShouldSplitCorrectly()
+    {
+        var context = GetDbContext();
+        var controller = new UAppGradeController(context);
+
+        var vm = new UappGradeVm
+        {
+            SplitData = new List<SplitData>
+            {
+                new SplitData { GradeId = 1, Ratio = 100m },
+            }
+        };
+
+        var result = controller.AddSplitRow(vm) as ViewResult;
+        var model = result.Model as UappGradeVm;
+
+        Assert.Equal(2, model.SplitData.Count);
+        Assert.Equal(100m, model.SplitData[0].Ratio);
+    }
+
+    [Fact]
+    public void Create_ValidModel_RedirectsToIndex()
+    {
+        var context = GetDbContext();
+        var controller = new UAppGradeController(context);
+
+        var vm = new UappGradeVm
+        {
+            GradeName = "Test Grade"
+        };
+
+        var result = controller.Create(vm);
+
+        Assert.IsType<RedirectToActionResult>(result);
+    }
+
+    [Fact]
+    public void Edit_InvalidModel_ReturnsView()
+    {
+        var context = GetDbContext();
+        var controller = new UAppGradeController(context);
+
+        controller.ModelState.AddModelError("GradeName", "Required");
+
+        var vm = new UappGradeVm();
+
+        var result = controller.Edit(vm);
+
+        Assert.IsType<ViewResult>(result);
+    }
+}
